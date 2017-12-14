4e7be4d29f0329e4c71e6288e2d7a8e4bdfe72cd
Asad Ibrahim <fahadi1997@gmail.com>
2017-12-14 16:07:56 -0500

    Add files via upload

--- final-project.js
+++ final-project.js
@@ -1,3 +1,24 @@
-$("#video-carousel-example").hover(function () {
-  $("video").get(0).play;
-})
+$(document).ready(function() {
+//pauses all videos when the page is loaded
+  $('video').each(function() {
+    $(this).get(0).pause();
+  });
+//when the mouse is over the carousel it will play the video according to the active slide in view
+  document.getElementById('video-carousel-example').addEventListener('mouseover', function() {
+    if ($('.videoCarousel0').hasClass('active')) {
+      $('.video0').get(0).play();
+    }
+    else if ($('.videoCarousel1').hasClass('active')) {
+      $('.video1').get(0).play();
+    }
+    else if ($('.videoCarousel2').hasClass('active')) {
+      $('.video2').get(0).play();
+    }
+  });
+//when the mouse is out of the carousel
+  document.getElementById('video-carousel-example').addEventListener('mouseout', function() {
+    $('video').each(function() {
+      $(this).get(0).pause();
+    });
+  });
+});
